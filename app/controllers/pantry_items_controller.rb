831044450eca59903fc6ee0d4185740add429731
jeremydimes <j10erten@gmail.com>
2022-11-15 11:44:33 -0700

    used jb templates

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,17 +1,17 @@
 class PantryItemsController < ApplicationController
   def index
-    pantryitems = current_user.pantry_items
-    render json: pantryitems.as_json
+    @pantry_items = current_user.pantry_items
+    render template: "pantry_items/index"
   end
 
   def create
-    pantry_item = current_user.pantry_items.find_by(ingredient_id: params[:ingredient_id])
-    if !pantry_item
-      pantry_item = PantryItem.create(
+    @pantry_item = current_user.pantry_items.find_by(ingredient_id: params[:ingredient_id])
+    if !@pantry_item
+      @pantry_item = PantryItem.create(
         ingredient_id: params[:ingredient_id],
         user_id: current_user.id,
       )
-      render json: pantry_item.as_json
+      render template: "pantry_items/show"
     else
       render json: { message: "already exists" }, status: 422
     end
